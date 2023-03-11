afac956dede2e6b4c8ba94df32d0b2920a778d94
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-03-11 19:20:14 +0530

    bcrypt(fifth commit)

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,62 @@
 
 const User = require('../models/createtable.js');
+const bcrypt = require('bcrypt')
 
-//  function isstringValid(string){
-// if(string == undefined || string.length ===0 ){
-//  return true
-// }else{
-//   return false
-// }
-// }
+  function isstringValid(string){
+ if(string == undefined || string.length ===0 ){
+  return true
+ }else{
+   return false
+}
+}
 
  exports.signup = async(req,res)=>{ 
   try {
      const {name, email, password} = req.body;
-      if(name == null || name.length ===0|| email == null|| email.length ===0||password == null ||
-         password.length ===0){                                                                 // we used this code for security purpose because frontened could be hacked
-         return res.status(400).json({err: "something is missing"})
-        }
-       await User.create({name, email, password})
+      // if(name == null || name.length ===0|| email == null|| email.length ===0||password == null ||
+      //    password.length ===0){                                                                 // we used this code for security purpose because frontened could be hacked
+      //    return res.status(400).json({err: "something is missing"})
+      //   }
+
+      if(isstringValid(name)|| isstringValid(email)|| isstringValid(password)){
+        res.status(400).json({msg: 'something is missing'})
+      }
+        const saltrounds = 10
+        bcrypt.hash(password, saltrounds, async(err, hash)=>{
+          console.log(err)
+       await User.create({name, email, password: hash})
         res.status(201).json({msg : 'successful'})
+        })
        }catch(err){
           res.status(500).json(err)
        }
 
    }
 
-   exports.login = (req, res)=>{
+   exports.login = async(req, res)=>{
+    try{
     const {email, password} = req.body;
+    if(isstringValid(name)|| isstringValid(email)|| isstringValid(password)){
+      res.status(400).json({msg: 'something is missing'})
+    }
     console.log(password)
-    User.findAll({where: {email}}).then(user =>{
-      if(user.length>0){
-        if(user[0].password == password){
+     const user = await User.findAll({where: {email}})
+        if(user.length>0){
+         bcrypt.compare(password, user[0].password, (err, response)=>{
+        if(response == true){
            res.status(200).json({success: true, message: 'user logged in successfully'})
-        }
+          }
+        else if(err){
+                return res.status(500).json({message: 'something went wrong'})
+          }
         else{
           return res.status(400).json({success: false, message: 'password is incorrect'})
         }
-      }else{
+       })
+       }else{
         return res.status(404).json({success: false, message: 'does not exist'})
-      }
-    }).catch(err =>{
+       }
+       }catch(err){
       res.status(500).json({message: err, success: false})
-    })
-   }
+      }
+      }
