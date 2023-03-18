e52ce3176f57a8b3670d2e62ccbdce18b83451b1
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-03-18 16:13:51 +0530

    optimised leaderboard

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -5,25 +5,46 @@ const e = require('express');
 
 const getUserLeaderBoard = async (req, res) => {
     try{
-        const users = await User.findAll()
-        const expenses = await Expense.findAll()
-        const userAggregateExpenses = {} // object
-        expenses.forEach(expenseAmount => {
-            if(userAggregateExpenses[expenseAmount.userId]){
-            userAggregateExpenses[expenseAmount.userId] =  userAggregateExpenses[expenseAmount.userId]+ expenseAmount.expense
-            }else{
-                userAggregateExpenses[expenseAmount.userId] = expenseAmount.expense
-            }
-        })
-         var UserLeaderBoardDetails = []
-          users.forEach((user)=>{
-              UserLeaderBoardDetails.push({name: user.name, total_cost: userAggregateExpenses[user.id] || 0})
-          })
+        
+            
+            const leaderboardofusers = await User.findAll({
+                attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expense')), 'total_cost'] ],
+                include: [
+                    {
+                        model: Expense,
+                        attributes: []
+                    }
+                ],
+                group:['user.id'],
+                order:[['total_cost', 'DESC']]
+    
+            })
+           
+            res.status(200).json(leaderboardofusers)
+
+        
+    
+        // const expenses = await Expense.findAll({
+        //     attributes: ['userId',[sequelize.fn('sum', sequelize.col(Expense.expense)), 'total_cost']],
+        //     group: ['userId']
+        // })
+        // const userAggregateExpenses = {} // object
+        // expenses.forEach(expenseAmount => {
+        //     if(userAggregateExpenses[expenseAmount.userId]){
+        //     userAggregateExpenses[expenseAmount.userId] =  userAggregateExpenses[expenseAmount.userId]+ expenseAmount.expense
+        //     }else{
+        //         userAggregateExpenses[expenseAmount.userId] = expenseAmount.expense
+        //     }
+        // })
+        //  var UserLeaderBoardDetails = []
+        //   users.forEach((user)=>{
+        //       UserLeaderBoardDetails.push({name: user.name, total_cost: userAggregateExpenses[user.id] || 0})
+        //   })
 
 
-        console.log(UserLeaderBoardDetails)
-        UserLeaderBoardDetails.sort((a,b)=> b.total_cost - a.total_cost )
-        res.status(200).json(UserLeaderBoardDetails)
+        // console.log(UserLeaderBoardDetails)
+        // UserLeaderBoardDetails.sort((a,b)=> b.total_cost - a.total_cost )
+        // res.status(200).json(UserLeaderBoardDetails)
     
 } catch (err){
     console.log(err)
